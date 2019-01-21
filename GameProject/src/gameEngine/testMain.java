dc6888b6bb70c64714f8f86341eeafb9218d4d3f
twentyonecoders <30596454+twentyonecoders@users.noreply.github.com>
2019-01-21 20:29:12 +0100

    project 2

--- GameProject/src/gameEngine/testMain.java
+++ GameProject/src/gameEngine/testMain.java
@@ -1,5 +1,9 @@
 package gameEngine;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 
@@ -12,10 +16,13 @@ import renderEngine.Renderer;
 import shaders.StaticShader;
 import toolBox.MousePicker;
 
-public class Main {
+public class testMain {
 
+	public static int gold = 0;
+	
 	static Entity entity;
-	static Entity goldMine;
+	static GoldMine goldMine1;
+	//static GoldMine goldMine2;
 	static Entity kaserne;
 	
 	public static void main(String[] args) {
@@ -29,16 +36,23 @@ public class Main {
 		MousePicker picker = new MousePicker(renderer.getProjectionMatrix(), camera);
 		
 		setUp(shader, renderer, image);
+		output();
 		
 		while(!Display.isCloseRequested()) {
 			camera.move();
 			renderer.prepare();
 			shader.start();
 			shader.loadViewMatrix(camera);
+			if (Keyboard.isKeyDown(Keyboard.KEY_1)) {
+				goldMine1.takeGold();
+			}
+			if(Keyboard.isKeyDown(Keyboard.KEY_U)) {
+				goldMine1.upgrade();
+			}
 			renderGraphics(shader, renderer);
 			shader.stop();
 			picker.update();
-			kaserne.setPosition(new Vector3f(picker.getCurrentRay().x * 11.5f, picker.getCurrentRay().y * 11.5f, -10f));
+			//kaserne.setPosition(new Vector3f(picker.getCurrentRay().x * 11.5f, picker.getCurrentRay().y * 11.5f, -10f));
 			//System.out.println("Mouse Coords: " + picker.getCurrentRay());
 			DisplayManager.updateDisplay();
 		}
@@ -49,16 +63,27 @@ public class Main {
 		System.exit(0);
 	}
 	
+	public static void output(){
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {	
+				System.out.println("Gold: " + gold);
+		    }
+		}, 1*1000, 1*1000);
+	}
+	
 	public static void setUp(StaticShader shader, Renderer renderer, Image image) {
 		entity = image.createImage("Download", new Vector3f(0, 0, -10), 0, 0, 0, 1);
-		goldMine = image.createImage("Goldmine", new Vector3f(3, 0, -10), 0, 0, 0, 1);
+		goldMine1 = new GoldMine(new Vector3f(3, 0, -10), 0, 0, 0, 1);
+		//goldMine2 = new GoldMine(new Vector3f(6, 0, -10), 0, 0, 0, 1);
 		kaserne = image.createImage("Kaserne", new Vector3f(-3, 0, -10), 0, 0, 0, 1);
 	}
 	
 	public static void renderGraphics(StaticShader shader, Renderer renderer) {
 		renderer.render(entity, shader);
-		renderer.render(goldMine, shader);
+		renderer.render(goldMine1.getImage(), shader);
+		//renderer.render(goldMine2.getImage(), shader);
 		renderer.render(kaserne, shader);
 	}
 }
-//das ist ei  test comment
